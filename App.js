f9fefd9a44a0c84f5056a04c8d3512b7fcdf88eb
ASchwad <a.schoenenwald@gmx.de>
2021-12-27 22:16:21 +0100

    Add translations and showcase screen

--- App.js
+++ App.js
@@ -1,103 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Button, SafeAreaView, ScrollView, Text, View} from 'react-native';
 
-import {
-  Colors,
-  Header,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import i18n from 'i18next';
+import {useTranslation, initReactI18next, withTranslation} from 'react-i18next';
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: {
+        welcome: 'Welcome!',
+        food: '🍔',
+      },
+    },
+    de: {
+      translation: {
+        welcome: 'Willkommen!',
+        food: '🥨',
+      },
+    },
+    fr: {
+      translation: {
+        welcome: 'Bienvenue!',
+        food: '🥖',
+      },
+    },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  debug: true,
+});
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const {t} = useTranslation();
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: 'white',
+    flex: 1,
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            alignItems: 'center',
+            minHeight: '100%',
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
+          <Text
+            style={{
+              fontSize: 24,
+              width: '80%',
+              textAlign: 'center',
+              fontWeight: '300',
+              letterSpacing: 3,
+              flex: 1,
+            }}>
+            React Native Translation Showcase
+          </Text>
+          <View style={{flex: 3, alignItems: 'center'}}>
+            <Text style={{fontSize: 40}}>{t('welcome')}</Text>
+            <Text style={{fontSize: 60}}>{t('food')}</Text>
+            <Text>{'Current language key: ' + i18n.language}</Text>
+          </View>
+          <View style={{flex: 2}}>
+            <Button
+              title="Change to english"
+              onPress={() => i18n.changeLanguage('en')}
+            />
+            <Button
+              title="Change to german"
+              onPress={() => i18n.changeLanguage('de')}
+            />
+            <Button
+              title="Change to french"
+              onPress={() => i18n.changeLanguage('fr')}
+            />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default withTranslation()(App);
